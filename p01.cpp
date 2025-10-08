f08415e581a285df5506bfe9a300b9d4c712749e
Aman Chouhan <ak6927887@gmail.com>
2025-10-08 11:25:44 +0530

    Create p01.cpp

---
+++ p01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// Function to calculate factorial using recursion
+int factorial(int n) {
+    if (n == 0 || n == 1)   // Base case
+        return 1;
+    else                    // Recursive case
+        return n * factorial(n - 1);
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "Factorial of " << num << " is: " << factorial(num) << endl;
+
+    return 0;
+}
