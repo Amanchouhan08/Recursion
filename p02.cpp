65f0e93114e8a6400c8ae2597f4fd06f0374a736
Aman Chouhan <ak6927887@gmail.com>
2025-10-10 23:04:12 +0530

    Update p02.cpp

--- p02.cpp
+++ p02.cpp
@@ -1,1 +1,21 @@
+//Sum of First N Natural Numbers.
+#include <iostream>
+using namespace std;
 
+int sum(int n) {
+    if (n == 0)
+        return 0;
+    return n + sum(n - 1);
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "Sum of first " << n << " numbers is: " << sum(n);
+}
+/*
+output
+Enter a number: 5
+Sum of first 5 numbers is: 15
+*/
