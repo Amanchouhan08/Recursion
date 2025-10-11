9244f13c88b4b1562febaa80ca1bdc6814a9d491
Aman Chouhan <ak6927887@gmail.com>
2025-10-11 17:19:10 +0530

    Create p06.cpp

---
+++ p06.cpp
@@ -0,0 +1,17 @@
+//Find Power of a Number (aⁿ)
+#include <iostream>
+using namespace std;
+
+int power(int a, int n) {
+    if (n == 0)
+        return 1;
+    return a * power(a, n - 1);
+}
+
+int main() {
+    int a, n;
+    cout << "Enter base and power: ";
+    cin >> a >> n;
+    cout << a << "^" << n << " = " << power(a, n);
+}
+
